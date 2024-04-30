9f25d034a55112db2d7443962391bd62ef9cb542
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-30 17:08:28 +0000

    task12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: {...employeeList},
+    allEmployees: {...employeesList},
     getNumberOfDepartments() {
       return Object.keys(employeesList).length;;
     },
