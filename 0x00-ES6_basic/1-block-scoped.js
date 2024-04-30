f9b5799a44f8077280252efc419ef5c14f7b524e
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-30 12:41:38 +0000

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task1 = true;
-    let task3 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
