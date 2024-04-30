60921f8b0b4de723c77cabccb041e03ab525ef8d
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-30 14:37:40 +0000

    task9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
@@ -12,4 +12,4 @@ export default function getFullBudgetObject(income, gdp, capita) {
     },
   };
   return fullBudget;
-};
+}
