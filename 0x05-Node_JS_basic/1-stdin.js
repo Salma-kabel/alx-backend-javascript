a98099427ef674481fade781073088e664e59dd0
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-04 19:44:41 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,9 @@ const rl = readline.createInterface({
 });
 console.log('Welcome to Holberton School, what is your name?');
 rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+  if (input.trim() !== '') {
+    console.log(`Your name is: ${input}`);
+  }
 });
 rl.on('close', () => {
   console.log('This important software is now closing');
