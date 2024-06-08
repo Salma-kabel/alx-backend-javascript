34d97c84d88f6a33563ffd8ca1bba4e6846425e4
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-08 18:18:54 +0000

    task13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,8 @@
-export default function createIteratorObject(report) {
-  return (function* () {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
+export default function* createIteratorObject(report) {
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      yield employee;
     }
-  })();
+  }
 }
+
