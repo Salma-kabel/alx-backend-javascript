2a401e1395e0900583bb6333b1ea5334a7da5553
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 23:15:47 +0000

    task4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   return arr
-    .filter((el) => el.location === city)
+    .filter((x) => el.location === city)
     .map((person) => {
-      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grades = newGrades.filter((x) => x.studentId === person.id);
       const grade = grades.length ? grades[0].grade : 'N/A';
       return {
         ...person,
