95ee56e769df0ba58bb809eb77c3790c953c0c7c
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 22:16:08 +0000

    task6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,7 +2,6 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof floors !== 'number') throw new Error();
     super(sqft);
     this._floors = floors;
     this._sqft = sqft;
