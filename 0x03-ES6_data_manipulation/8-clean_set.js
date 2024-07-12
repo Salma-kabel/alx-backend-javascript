f4cd0305843facd97a939948a12bcbb18c981d44
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 23:23:35 +0000

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@
 export default function cleanSet(set, startString) {
   let res = '';
   if (!startString || !startString.length) return res;
-  for (const el of set) {
-    if (el && el.startsWith(startString)) {
-      res += `${el.slice(startString.length)}-`;
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      res += `${element.slice(startString.length)}-`;
     }
   }
   return res.slice(0, res.length - 1);
