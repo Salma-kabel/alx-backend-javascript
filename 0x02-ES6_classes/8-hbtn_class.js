f22289f0f2183f557eb75c02c2992330a22f7192
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 22:21:25 +0000

    task8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,15 +1,14 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number' || typeof location !== 'string') throw new Error();
     this._size = size;
     this._location = location;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'string') {
+  [Symbol.toPrimitive](x) {
+    if (x === 'string') {
       return this._location;
     }
-    if (hint === 'number') {
+    if (x === 'number') {
       return this._size;
     }
     return this;
