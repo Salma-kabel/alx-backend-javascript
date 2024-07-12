ff3a640a7ec678becc57db5a3e46a13de56371a5
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-12 21:52:45 +0000

    task9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,6 @@
 export default function guardrail(mathFunction) {
   try {
-    const res = mathFunction();
-    return [res, 'Guardrail was processed'];
+    return [mathFunction(), 'Guardrail was processed'];
   } catch (err) {
     return [err.message, 'Guardrail was processed'];
   }
