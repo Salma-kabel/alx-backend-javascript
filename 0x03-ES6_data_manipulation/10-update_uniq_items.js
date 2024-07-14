9730ec377002b2965158c42be2c3aff352b16417
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-14 19:39:35 +0000

    task10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,13 @@
 const updateUniqueItems = (map1) => {
-  if (!(map1 instanceof Map)) throw new Error('Cannot process');
+  if (!(map1 instanceof Map))
+    throw new Error('Cannot process');
 
-  map1.forEach((value, key) => {
-    if (value === 1) map1.set(key, 100);
-  });
+  for (const [key, value] of getMap) {
+    if (value === 1) {
+      map1.set(key, 100);
+    }
+  }
+  return map1;
 };
 
 export default updateUniqueItems;
