0556dd76b788dfb2e66d65e460a3f150f8c5c526
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-14 19:42:38 +0000

    task11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 const weakMap = new WeakMap();
 
-const queryAPI = (endpoint) => {
-  let total = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, total -= -1);
-  if (total >= 5) throw new Error('Endpoint load is high');
-  return total;
+const queryAPI = (endPoint) => {
+  let queries = weakMap.get(endPoint) || 0;
+  weakMap.set(endPoint, queries -= -1);
+  if (queries >= 5) throw new Error('Endpoint load is high');
+  return queries;
 };
 
 export { weakMap, queryAPI };
